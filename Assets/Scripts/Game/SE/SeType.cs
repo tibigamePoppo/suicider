95cdc1af4d1147b4d219686c6e0827b7c4a2f8ca
tibigameMarl <ryosukehashimoto1@gmail.com>
2023-06-06 04:07:26 +0900

    素材及びマネージャーの制作

---
+++ Assets/Scripts/Game/SE/SeType.cs
@@ -0,0 +1,15 @@
+
+/// <summary>
+/// SE‰¹Œ¹‚ÌŽí—Þ
+/// </summary>
+
+namespace System.Audio
+{
+    public enum SeType
+    {
+        Dead,
+        Goal,
+        ScrollOpen,
+        Select
+    }
+}
